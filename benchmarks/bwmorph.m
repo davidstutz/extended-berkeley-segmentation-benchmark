6321a03e6fc533a3608911a489b9904149ee1387
arslankhan8 <arslankhan52@gmail.com>
2017-07-31 16:01:55 +0500

    Updating function header

--- benchmarks/bwmorph.m
+++ benchmarks/bwmorph.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2017 Arslan Khan <brandon.miles7@gmail.com>, Muhammad Talha <>
+## Copyright (C) 2017  Arslan Khan <arslankhan52@gmail.com>, Muhammad Talha <m.talha.imran01@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
@@ -14,24 +14,15 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*- 
-## @deftypefn  {Function File} {[@var{gradMag}, @var{gradDir}] =} imgradient (@var{img})
-## @deftypefnx {Function File} {[@var{gradMag}, @var{gradDir}] =} imgradient (@var{img}, @var{method})
-## @deftypefnx {Function File} {[@var{gradMag}, @var{gradDir}] =} imgradient (@var{gx}, @var{gy})
-## Compute the gradient magnitude and direction in degrees for an image.
+## gradMag = bwmorph(image)
+## where image is the binary image, and grad image is the morphological operation
 ##
-## These are computed from the @var{gx} and @var{xy} gradients using
-## @code{imgradientxy}.  The first input @var{img} is a gray scale image to
-## compute the edges on.  The second input @var{method} controls the method
-## used to calculate the gradients.  Alternatively the first input @var{gx}
-## can be the x gradient and the second input @var{gy} can be the y gradient.
+## This function implements a minimum implementation of bwmorph enough to carry 
+## out benchmarking. This helper API performs Morphological operations on 
+## binary images
+
+## The first output @var{gradMag} returns image skeleton
 ##
-## The first output @var{gradMag} returns the magnitude of the gradient.
-## The second output @var{gradDir} returns the direction in degrees.
-##
-## The @var{method} input argument must be a string specifying one of the
-## methods supported by @code{imgradientxy}.
-##
-## @seealso{edge, imgradientxy}
 ## @end deftypefn
 
 function [gradMag] = bwmorph (img)
@@ -45,31 +36,4 @@ function [gradMag] = bwmorph (img)
     end
 
 endfunction
-
-%!test
-%! A = [0 1 0
-%!      1 1 1
-%!      0 1 0];
-%!
-%! [gMag, gDir] = imgradient (A);
-%! assert (gMag,[sqrt(18) 4 sqrt(18); 4 0 4; sqrt(18),4,sqrt(18)]);
-%! assert (gDir,[-45 -90 -135; -0 -0 -180; 45 90 135]);
-%!
-%! ## the following just test if passing gx and gy separately gets
-%! ## us the same as the image and method though imgradient
-%! [gxSobel, gySobel] = imgradientxy (A, "Sobel");
-%! [gxPrewitt, gyPrewitt] = imgradientxy (A, "Prewitt");
-%! [gxCd, gyCd] = imgradientxy (A, "CentralDifference");
-%! [gxId, gyId] = imgradientxy (A, "IntermediateDifference");
-%!
-%! assert (imgradient (A),
-%!         imgradient (gxSobel, gySobel));
-%! assert (imgradient (A, "Sobel"),
-%!         imgradient (gxSobel, gySobel));
-%! assert (imgradient (A, "Prewitt"),
-%!         imgradient(gxPrewitt, gyPrewitt));
-%! assert (imgradient (A, "CentralDifference"),
-%!         imgradient (gxCd, gyCd));
-%! assert (imgradient (A, "IntermediateDifference"),
-%!         imgradient (gxId, gyId));
 
